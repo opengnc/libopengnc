780cd0a279d2a497027ff4849559561ed0bdf26a
rormerod <richard.ormerod@newcastle.edu.au>
2015-08-11 08:43:04 +1000

    Removed measurement service template, added measurement density type and measurement exclusion policy.

--- include/opengnc/estimation/measurement_update.hpp
+++ include/opengnc/estimation/measurement_update.hpp
@@ -7,13 +7,13 @@
 namespace opengnc {
 namespace estimation {
 
-template<typename density_type,
+template<typename state_density_type,
+         typename measurement_density_type,
          typename measurement_model_type,
-         typename measurement_service_type,
          typename propagater_type,
-         typename constraint_policy>
+         typename state_constraint_policy,
+         typename measurement_exclusion_policy>
 class measurement_update
-        : constraint_policy
 {
     enum { nx = measurement_model_type::input_length, ny = measurement_model_type::output_length };
 
@@ -25,15 +25,14 @@ class measurement_update
     typedef Eigen::Matrix<output_scalar_type, nx, ny> xy_mat;
 
 public:
-    measurement_update(measurement_service_type& measurement_service, measurement_model_type& model)
-        : _measurement_service(measurement_service)
-        , _model(model)
+    measurement_update(measurement_model_type& model)
+        : _model(model)
     { }
 
-    void operator () (density_type& density)
+    void operator () (state_density_type& state_density, const measurement_density_type& measurement_density)
     {
-        const auto& x = density.mean();
-        const auto& Px = density.covariance();
+        const auto& x = state_density.mean();
+        const auto& Px = state_density.covariance();
 
         y_vec y_hat;
         y_mat Py_hat;
@@ -42,35 +41,34 @@ public:
         _propagater.init(x, Px);
         _propagater.propagate(_model, y_hat, Py_hat, Pxy_hat);
 
-        _y_hat = y_hat;
-
-        y_vec y = _measurement_service.get_measurement();
-        y_mat R = _measurement_service.get_measurement_uncertainty();
+        y_vec y = measurement_density.mean();
+        const y_mat& R = measurement_density.covariance();
 
         if(y.rows() > 0 && y_hat.rows() > 0)
         {
             //Add measurement noise
             Py_hat +=  R;
+            measurement_exclusion_policy::apply_exclusions(y, y_hat, Py_hat, Pxy_hat);
         }
+
+        _y_hat = y_hat;
 
         if(y.rows() > 0 && y_hat.rows() > 0)
         {
             //Conditioning: K = Pxy/Py
             xy_mat K = Py_hat.ldlt().solve(Pxy_hat.transpose()).transpose();
 
-            density.mean() = x + (K*(y - y_hat)).template cast<typename density_type::scalar_type>();
-            density.covariance() = Px - (K*Py_hat*K.transpose()).template cast<typename density_type::scalar_type>();
+            state_density.mean() = x + (K*(y - y_hat)).template cast<typename state_density_type::scalar_type>();
+            state_density.covariance() = Px - (K*Py_hat*K.transpose()).template cast<typename state_density_type::scalar_type>();
 
             //Apply Constaints
-            constraint_policy::apply_constraints(density.mean());
+            state_constraint_policy::apply_constraints(state_density.mean());
         }
     }
 
     const y_vec& predicted_measurement() const { return _y_hat; }
 
-
 private:
-    measurement_service_type& _measurement_service;
     measurement_model_type& _model;
     propagater_type _propagater;
     y_vec _y_hat;
